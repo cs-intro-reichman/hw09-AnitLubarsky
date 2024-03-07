e5ae3c5c9498fa0d662b502a708521c408bc942b
Anit Lubarsky <anit.lubarsky@runi.ac.il>
2024-03-07 12:08:13 +0200

    hw09_2

--- LanguageModel.java
+++ LanguageModel.java
@@ -32,39 +32,50 @@ public class LanguageModel {
     }
 
     /** Builds a language model from the text in the given file (the corpus). */
-	// public void train(String fileName) {
-    //     public void train(String fileName) {
-    //         String window = "";
-    //         char c;
-    //         In in = new In(fileName);
-    //         // Reads just enough characters to form the first window
-    //         code: Performs the action described above.
-    //         // Processes the entire text, one character at a time
-    //         while (!in.isEmpty()) {
-    //         // Gets the next character
-    //         c = in.readChar();
-    //         // Checks if the window is already in the map
-    //         code: tries to get the list of this window from the map.
-    //         Let’s call the retrieved list “probs” (it may be null)
-    //         // If the window was not found in the map
-    //         code: the if statement described above {
-    //         // Creates a new empty list, and adds (window,list) to the map
-    //         code: Performs the action described above.
-    //         Let’s call the newly created list “probs”
-    //         }
-    //         // Calculates the counts of the current character.
-    //         probs.update(c);
-    //         // Advances the window: adds c to the window’s end, and deletes the
-    //         // window's first character.
-    //         code: Performs the action described above.
-    //         }
-    //         // The entire file has been processed, and all the characters have been counted.
-    //         // Proceeds to compute and set the p and cp fields of all the CharData objects
-    //         // in each linked list in the map.
-    //         for (List probs : probabilities.values())
-    //         calculateProbabilities(probs);
-    //         }
-       /*  List list;		        
+	 public void train(String fileName) {
+        List list;		        
+        int windowLength = 2;
+   
+        String window = "";
+        StringBuffer strBuilder = new StringBuffer();
+        char c;
+        In in = new In(fileName);
+        // Reads just enough characters to form the first window
+        //code: Performs the action described above.
+        // Processes the entire text, one character at a time
+        while (!in.isEmpty()) {
+            // Gets the next character   
+            if (strBuilder.length() < windowLength) {
+                c = in.readChar();
+                strBuilder.append(c);
+                continue;
+            }
+            if (!in.isEmpty()){
+                c = in.readChar();
+            }else{
+                break;
+            }
+            window = strBuilder.toString();
+            if(this.CharDataMap.containsKey(window) == true){
+                this.CharDataMap.get(window).update(c);
+            } else {
+                list = new List();
+                list.update(c);
+                this.CharDataMap.put(strBuilder.toString(), list);
+            }
+            strBuilder.append(c);
+            strBuilder.delete(0,1);
+        }
+            // The entire file has been processed, and all the characters have been counted.
+            // Proceeds to compute and set the p and cp fields of all the CharData objects
+            // in each linked list in the map.
+        List keyProbs;
+        for (String key : CharDataMap.keySet()) {             
+			keyProbs = CharDataMap.get(key);
+			calculateProbabilities(keyProbs);
+            System.out.println(key + " " + keyProbs);
+		}
+     /*    List list;		        
         int windowLength = 2;
         String subStr;
         char ch;
@@ -89,7 +100,8 @@ public class LanguageModel {
 		}
 
        // System.out.println(this.CharDataMap.toString());
-	}*/
+       */
+	}
 
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
@@ -153,15 +165,15 @@ public class LanguageModel {
 	}
 
     public static void main(String[] args) {      
-        List list = new List();
+       // List list = new List();
         //String str = "you cannot teach a man anything; you can only help him find it within himself.";
         // list.addFirst(' ');
         // for (int i = str.length()-1; i >=0; i--) {
         //     list.update(str.charAt(i));
         // }
-        LanguageModel lm = new LanguageModel(0);
+        LanguageModel lm = new LanguageModel(2);
 
-        lm.train(str);
+        lm.train("galileo.txt");
        // System.out.println("remove: " + list.remove('h') );
       //  System.out.println("char data: " + list.get(2).toString());
        // list.update('H');
